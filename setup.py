f4924dc1a4b400026c736c5d851a0bfd5be6c15e
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-12-08 09:13:29 +0530

    setup packages

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name="Language_Identification_Using_CNN",
+    author_name="Abhishek",
+    author_email="abhishek.jadhav2@pw.live",
+    packages=find_packages(),
+    install_requires=[])
